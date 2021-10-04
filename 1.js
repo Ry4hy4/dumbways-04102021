8e6e5aad265487661e353a8d4f6f7b5f16e697c0
Ahmad Raihannudin Yahya <71114809+Ry4hy4@users.noreply.github.com>
2021-10-04 15:37:14 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,12 @@
 const distanceCount= () => {
   const distanceOffice = 64500;
   let distanceInMeter = distanceOffice;
-  let speed
+  let speed;
+  let resultTime;
   // first 23 minute
   speed = 3;
   time = 23 * 60;
+  resultTime = time;
   distanceIn23minute = speed * time;
   distanceInMeter -= distanceIn23minute;
 
@@ -12,10 +14,21 @@ const distanceCount= () => {
 
   speed = 5;
   time = 12 * 60;
+  resultTime += time;
   distanceInsecond12Minute = speed * time;
   distanceInMeter -= distanceInsecond12Minute;
 
   //last speed
+  const remainingDistance = distanceOffice - distanceInMeter;
+  speed = 3;
+
+  time = remainingDistance / speed;
+  resultTime += time;
+
+  const jam = Math.floor(resultTime/60/60);
+  const menit = Math.floor(resultTime/60-60);
+  const detik = resultTime - (jam * 60 * 60) - (menit * 60);
+  console.log(`Waktu yang ditempuh ${jam}jam, ${menit}Menit, ${detik} detik`);
 }
 
-distanceCount();
+distanceCount()
